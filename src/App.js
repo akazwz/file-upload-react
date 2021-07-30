acf14ca56089534f019f66197be673e2fbde5cbe
akez <1791189210@qq.com>
2021-07-30 11:46:56 +0800

    编写完成输入选择框组件

--- src/App.js
+++ src/App.js
@@ -2,11 +2,24 @@ import './App.css';
 import FileUpload from './pages/FileUpload';
 import ComBox from './components/ComBox';
 
-const App = () => (
-    <div>
-        <FileUpload/>
-        <ComBox/>
-    </div>
-)
+
+const App = () => {
+    const arr = [
+        13456453, 25345345, 7456756, 7682454, 34534534,
+        3453476768, 78956756878, 34578933, 7897899789,
+        'huanying', 'dajia', 'geige', 'star',
+        'he', 'follow', 'my', 'github', 'is', 'akazwz',
+        '欢迎', '大家', '给个', 'star',
+    ];
+    return (
+        <div>
+            <FileUpload/>
+            <div style={{marginLeft: 50}}>
+                <ComBox data={arr}/>
+            </div>
+        </div>
+    );
+};
+
 
 export default App;
--- src/components/ComBox.js
+++ src/components/ComBox.js
@@ -1,35 +1,104 @@
-import React, {useRef, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
+import {findDOMNode} from "react-dom";
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import IconButton from '@material-ui/core/IconButton';
 
-const ComBox = () => {
+const ComBox = (props) => {
     const [hidden, setHidden] = useState(true);
+    const [value, setValue] = useState('');
     const [options, setOptions] = useState(true);
+    const inputRef = useRef();
+    const btnRef = useRef();
     const optionsDiv = useRef();
-    const handleInputChange = () => {
+    const handleInputChange = (e) => {
         setHidden(false);
-        addOptions();
+        setValue(e.target.value);
+        const arr = props.data
+        let newArr = [];
+        for (let i = 0; i < arr.length; i++) {
+            let str = arr[i].toString()
+            if (str.includes(e.target.value)) {
+                newArr.push(arr[i]);
+            }
+        }
+        addOptions(newArr);
     };
     const handleSelectClick = () => {
         setHidden(false);
     };
-    const addOptions = () => {
-        const optionsAdd = <div>
-            <option onClick={handleOptionsClick}>1</option>
-            <option onClick={handleOptionsClick}>2</option>
-            <option onClick={handleOptionsClick}>3</option>
-            <option onClick={handleOptionsClick}>4</option>
-        </div>;
-        setOptions(optionsAdd);
+    // 添加提示选项
+    const addOptions = (arr) => {
+        let opt = [];
+        for (let i = 0; i < arr.length; i++) {
+            opt.push(<option key={i} onClick={handleOptionsClick}>{arr[i]}</option>);
+        }
+        setOptions(opt);
     };
+    // 选择点击
     const handleOptionsClick = (e) => {
-        alert(e.target.value);
+        setValue(e.target.value);
+        setHidden(true);
     };
+    // 外部点击
+    const handleClickOutsideInput = (e) => {
+        // node 实例
+        const input = findDOMNode(inputRef.current);
+        const btn = findDOMNode(btnRef.current);
+        const div = findDOMNode(optionsDiv.current);
+
+        // 判断点击是否在 input btn div 的外部
+        if (e.target !== input && e.target !== btn && !div.contains(e.target)) {
+            setHidden(true);
+        }
+    }
+    // 添加点击事件
+    useEffect(() => {
+        const arr = props.data;
+        addOptions(arr);
+        document.addEventListener('mousedown', (e) => {
+            handleClickOutsideInput(e);
+        })
+        // eslint-disable-next-line
+    },[]);
     return (
-        <div>
-            <input placeholder="请输入" style={{width: 200}} onInput={handleInputChange}/>
-            <button style={{width: 10, marginLeft: -20, border: "none"}} onClick={handleSelectClick}>
-                {">"}
-            </button>
-            <div ref={optionsDiv} style={{width: 200, backgroundColor: "red", zIndex: 1000}} hidden={hidden}>
+        <div style={{width: 200,height: 60}}>
+            <input
+                placeholder="请输入"
+                style={{
+                    width: 165,
+                    height: 40,
+                    background: '#FFFFFF',
+                    borderRadius: 2,
+                    border: '1px solid #CCCCCC',
+                    fontSize: 12,
+                    fontWeight: 400,
+                    color: '#AAAAAA',
+                    display: 'inline',
+                    padding: 5,
+                }}
+                value={value}
+                onInput={handleInputChange}
+                ref={inputRef}
+            />
+            <IconButton style={{marginLeft: -40}} ref={btnRef} onClick={handleSelectClick}>
+                <ExpandMoreIcon/>
+            </IconButton>
+            {/*提示输入,下拉选择 options*/}
+            <div
+                ref={optionsDiv}
+                style={{
+                    width: 165,
+                    height: 300,
+                    background: '#FFFFFF',
+                    borderRadius: 2,
+                    border: '1px solid #CCCCCC',
+                    fontSize: 12,
+                    fontWeight: 400,
+                    color: '#AAAAAA',
+                    zIndex: 1000,
+                    overflow: 'scroll'
+                }}
+                hidden={hidden}>
                 {options}
             </div>
         </div>
