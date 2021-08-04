78f2034ddd6fd3699b32604434f14f9bee6c5fa3
akez <1791189210@qq.com>
2021-08-04 17:46:01 +0800

    控制滚动条滚动

--- src/components/ScrollBar.js
+++ src/components/ScrollBar.js
@@ -2,25 +2,37 @@ import React from 'react';
 import '../css/scroll-bar.css';
 
 const ScrollBar = () => {
+    const handleBtnUpClick = () => {
+        document.getElementById('main').scrollTop += 60;
+    };
+
+    const handleBtnDownClick = () => {
+        document.getElementById('main').scrollTop -= 60;
+    };
+
     return (
-        <div className="container">
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-            <li>1</li>
-        </div>
+        <>
+            <div id="main" className="container">
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>4</li>
+                <li>5</li>
+                <li>6</li>
+                <li>7</li>
+                <li>8</li>
+                <li>9</li>
+                <li>10</li>
+                <li>11</li>
+                <li>12</li>
+                <li>13</li>
+                <li>14</li>
+                <li>15</li>
+                <li>16</li>
+            </div>
+            <button onClick={handleBtnUpClick}>Up</button>
+            <button onClick={handleBtnDownClick}>Down</button>
+        </>
     );
 };
 
